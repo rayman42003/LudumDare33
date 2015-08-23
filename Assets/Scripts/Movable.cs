0919525f5a21d8d65752b7c734cdc72c83e73fd1
Richmond Chang <rayman42003@gmail.com>
2015-08-22 21:15:32 -0700

    - Added all the art made so far - Added the Intro level - Added Movement script and Player Controls

---
+++ Assets/Scripts/Movable.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movable : MonoBehaviour {
+    public float moveSpeed;
+    public float jumpHeight;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask groundMask;
+    
+    public static readonly int numJumps = 1;
+    private Rigidbody2D body;
+    private int jumpsLeft;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        jumpsLeft = numJumps;
+    }
+
+    void FixedUpdate()
+    {
+        if (Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundMask))
+        {
+            if (jumpsLeft == 0)
+                Debug.Log("Jump Reset");
+            jumpsLeft = numJumps;
+        }
+    }
+
+    public void MoveRight()
+    {
+        body.velocity = new Vector2(moveSpeed, body.velocity.y);
+    }
+
+    public void MoveLeft()
+    {
+        body.velocity = new Vector2(-moveSpeed, body.velocity.y);
+    }
+
+    public void Jump()
+    {
+        if (jumpsLeft > 0)
+        {
+            body.velocity = new Vector2(body.velocity.x, jumpHeight);
+            --jumpsLeft;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+    private Movable player;
+	// Use this for initialization
+	void Start () {
+        player = GetComponent<Movable>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            player.MoveLeft();
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            player.MoveRight();
+
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            player.Jump();
+	}
+}
