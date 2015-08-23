abab99df2303bb85f644d898ef0b5f3774761f8b
Richmond Chang <rayman42003@gmail.com>
2015-08-23 15:56:54 -0700

    -  Added checkpoint script and made signposts checkpoints.

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Checkpoint : MonoBehaviour {
+
+    private LevelManager level;
+    private bool triggered;
+
+    void Start () {
+        level = FindObjectOfType<LevelManager>();
+        triggered = false;
+	}
+	
+	void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.name == "Player" && !triggered)
+        {
+            level.checkpoint = gameObject.transform;
+            triggered = true;
+        }
+	}
+}
